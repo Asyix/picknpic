ec5d7b465199b7e8c79a5d31fb26a0731b87239c
wayne <wayne.wan-chow-wah@etu.umontpellier.fr>
2025-01-12 19:31:07 +0100

    feat: dynamic rendering of elements done

--- src/main/java/fr/polytech/picknpic/ui/controllers/MainController.java
+++ src/main/java/fr/polytech/picknpic/ui/controllers/MainController.java
@@ -3,17 +3,9 @@ package fr.polytech.picknpic.ui.controllers;
 import fr.polytech.picknpic.bl.facades.user.LoginFacade;
 import fr.polytech.picknpic.bl.models.User;
 import fr.polytech.picknpic.ui.SceneManager;
-import fr.polytech.picknpic.ui.controllers.UserControllers.LoginController;
-import fr.polytech.picknpic.ui.controllers.RequestControllers.ManageRequestController;
-import fr.polytech.picknpic.ui.controllers.ServiceControllers.ManageServicesController;
-import fr.polytech.picknpic.ui.controllers.GradeControllers.ManageGradesController;
-import fr.polytech.picknpic.ui.controllers.UserControllers.RegisterController;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
 
 /**
  * Controller for the main application view.
@@ -44,24 +36,12 @@ public class MainController {
 
     // Temporary methods to navigate to other scenes
 
-    public void handleCreateRequest() {
-        sceneManager.loadCreateRequestScene();
-    }
-
     public void handleChangeRequestStatus() {
         sceneManager.loadChangeRequestStatusScene();
     }
 
     public void handleCreateService() {
         sceneManager.loadCreateServiceScene();
-    }
-
-    public void handleUpdateService() {
-        sceneManager.loadUpdateServiceScene();
-    }
-
-    public void handleDeleteService() {
-        sceneManager.loadDeleteServiceScene();
     }
 
     public void handleCreateGrade() {
